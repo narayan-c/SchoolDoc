9d533c15316cdf259e58c13008e2977d1bd7e555
Chinmay Narayan <Chinmay@amuselabs.com>
2023-08-17 23:29:54 +0530

    Title change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Parishkaaram Publi School',
+  title: 'Parishkaaram Public School',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
